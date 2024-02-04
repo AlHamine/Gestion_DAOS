c43844524e375518cb2543d49553939218c76b7e
AlHamine <oriontheroot@gmail.com>
2024-02-04 23:44:26 +0000

    ajout clase

---
+++ src/views/base/classe/Classe.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  // CTableCaption,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+  CButton,
+  CPagination,
+  CPaginationItem,
+} from '@coreui/react'
+import { SERVER_URL } from 'src/constantURL'
+import { Link } from 'react-router-dom'
+// import { DocsExample } from 'src/components'
+
+const Ue = () => {
+  const [listUE, setListUE] = useState([])
+
+  useEffect(() => {
+    fetchUE()
+  }, [])
+
+  const fetchUE = () => {
+    fetch(SERVER_URL + 'maquette/ue')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return response.json()
+      })
+      .then((data) => setListUE(data))
+      .catch((error) => console.error('Error fetching UE:', error))
+  }
+
+  const onDelClick = (id) => {
+    // console.log(typeof id)
+    if (window.confirm('Are you sure to delete?')) {
+      fetch(SERVER_URL + `maquette/ue/${id}`, { method: 'DELETE' })
+        .then((response) => {
+          if (response.ok) {
+            alert('UE supprimer')
+            fetchUE()
+          } else {
+            alert("Une erreur s'est produite lors de la suppression.")
+          }
+        })
+        .catch((err) => console.error(err))
+    }
+  }
+
+  return (
+    <CRow>
+      <div className="d-grid gap-2 col-6 mx-auto" style={{ marginBottom: '10px' }}>
+        <div className="text-center">
+          <Link to={'/base/ue/AddUe'}>
+            <CButton color="primary" style={{ fontWeight: 'bold' }}>
+              Ajouter un UE
+            </CButton>
+          </Link>
+        </div>
+      </div>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <strong>Liste </strong> <small>des UE</small>
+          </CCardHeader>
+          <CCardBody>
+            {/* <DocsExample href="components/table#table-head"> */}
+            <CTable>
+              <CTableHead color="dark">
+                <CTableRow>
+                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Code</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Libelle</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Credits</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Coefficient</CTableHeaderCell>
+                  <CTableHeaderCell scope="col" className="text-center">
+                    Operation
+                  </CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Details</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                {listUE.map((ue, index) => (
+                  <CTableRow key={index}>
+                    <CTableHeaderCell scope="row"> {index + 1} </CTableHeaderCell>
+                    <CTableDataCell>{ue.code}</CTableDataCell>
+                    <CTableDataCell>{ue.libelle}</CTableDataCell>
+                    <CTableDataCell className="text-center">{ue.credit}</CTableDataCell>
+                    <CTableDataCell className="text-center">{ue.coefficient}</CTableDataCell>
+                    <CTableDataCell className="text-center">
+                      <CButton color="primary" className="me-1">
+                        Modifier
+                      </CButton>
+                      <CButton color="danger" onClick={() => onDelClick(ue.id)}>
+                        Supprimer
+                      </CButton>
+                    </CTableDataCell>
+                    <CTableDataCell>
+                      <CButton color="info">Detail</CButton>
+                    </CTableDataCell>
+                  </CTableRow>
+                ))}
+                <CPagination align="end" aria-label="Page navigation example">
+                  <CPaginationItem disabled>Previous</CPaginationItem>
+                  <CPaginationItem>1</CPaginationItem>
+                  <CPaginationItem>2</CPaginationItem>
+                  <CPaginationItem>3</CPaginationItem>
+                  <CPaginationItem>Next</CPaginationItem>
+                </CPagination>
+              </CTableBody>
+            </CTable>
+            {/* </DocsExample> */}
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default Ue
