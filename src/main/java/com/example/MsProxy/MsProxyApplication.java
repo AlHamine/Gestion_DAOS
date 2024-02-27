13ee760dc2f8fdab61730c277281c5433f56520f
AlHamine <oriontheroot@gmail.com>
2024-02-27 18:15:19 +0000

    bon

--- src/main/java/com/example/MsProxy/MsProxyApplication.java
+++ src/main/java/com/example/MsProxy/MsProxyApplication.java
@@ -1,8 +1,7 @@
 package com.example.MsProxy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
